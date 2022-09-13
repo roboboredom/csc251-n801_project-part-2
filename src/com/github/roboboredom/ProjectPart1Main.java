a5f334970073b2f4844f5827766c2f1443d538c7
roboboredom <57413665+roboboredom@users.noreply.github.com>
2022-09-12 20:55:48 -0400

    Initial Content Commit
    n/a

---
+++ src/com/github/roboboredom/ProjectPart1Main.java
@@ -0,0 +1,16 @@
+/* Class: CSC251-N801: Advanced JAVA Programming
+ * Date: 9/10/2022
+ * Author: Willem Dawson Gray
+ * 
+ * Assignment: Project Part 1
+ */
+package com.github.roboboredom;
+
+public class ProjectPart1Main 
+{
+	public static void main(String[] args) 
+	{
+		System.out.print("Hello, world.\n");
+	}
+
+}
